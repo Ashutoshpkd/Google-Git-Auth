3a9e5af74484fca7140d57f6a8447710c464b7f0
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2023-04-24 09:22:38 +0530

    Animation setup

---
+++ layout/layout.js
@@ -0,0 +1,22 @@
+import styles from './layout.module.css';
+
+export default function Layout( { children }){
+    return (
+        <div className="flex h-screen bg-blue-400">
+            <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+                <div className={styles.img}>
+                    <div className={styles.cartoon}>
+                    </div>
+                    <div className={styles.cloud_one}></div>
+                    <div className={styles.cloud_two}></div>
+                </div>
+                <div className="right flex flex-col justify-evenly bg-gray-500">
+                    <div className="text-center py-10">
+                        {children}
+                    </div>
+                </div>
+            </div>
+  
+        </div>
+    )
+}
