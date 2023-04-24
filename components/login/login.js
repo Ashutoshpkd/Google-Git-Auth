d1532649012ca96225cc9f76fa03bd466112b363
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2023-04-24 11:19:10 +0530

    Styling the input  form and structuring the code

---
+++ components/login/login.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import styles from './login.module.css';
+import Image from 'next/image';
+
+export default function Login(){
+    return (
+        <section className='w-3/4 mx-auto flex flex-col gap-10'>
+            <div className='title'>
+                <h1 className='text-gray-800 text-4xl font-bold py-4'>Explore</h1>
+                <p className='w-3/4 mx-auto text-gray-400'>My name is Ashutosh</p>
+            </div>
+            <form className='flex flex-col gap-5'> 
+                <div className={styles.input_group}>
+                    <input 
+                        type='email'
+                        name='email'
+                        placeholder='Email'
+                        className={styles.input_text}
+                    />
+                </div>
+                <div className={styles.input_group}>
+                    <input 
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                        className={styles.input_text}
+                    />
+                </div>
+                <div className={styles.button}>
+                    <button type='submit'>Login</button>
+                </div>
+                <div className='input-button'>
+                    <button type='button' className={styles.button_custom}>
+                        Sign in with Google
+                        <Image src={'/assets/google.svg'} width={20} height={20}/>
+                    </button>
+                </div>
+                <div className='input-button'>
+                    <button type='submit' className={styles.button_custom}>
+                        Sign in with Github
+                        <Image src={'/assets/github.svg'} width={25} height={25}/>
+                    </button>
+                </div>
+                <p className='text-center text-gray-400'>
+                    don't have an account yet? <Link href='/register'><a className='text-blue-400'>Sign up</a></Link>
+                </p>
+            </form>
+        </section>
+    )
+}
--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,14 @@
 import Head from 'next/head'
 import Layout from '../layout/layout'
+import LoginForm from '../components/login/login';
 
 export default function Login(){
     return (
         <Layout>
-
         <Head>
             <title>Login</title>
         </Head>
-
-
-            <h1>Login</h1>
+        <LoginForm />
         </Layout>
     )
 }
